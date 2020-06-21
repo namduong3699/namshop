bb516a117f877fef3cdd352bfa205ae2c853d124
Dương Hoài Nam <33230044+namduong3699@users.noreply.github.com>
2020-06-22 00:18:43 +0700

    Update Confirm.php

--- app/Models/Confirm.php
+++ app/Models/Confirm.php
@@ -3,12 +3,9 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Confirm extends Model
 {
-	use SoftDeletes;
-
 	protected $table="confirm_user";
 
 	public  $timestamps = true;
