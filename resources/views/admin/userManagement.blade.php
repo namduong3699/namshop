badfd59006f69a90abc4d95e3a590f1d02e2a531
Dương Hoài Nam <33230044+namduong3699@users.noreply.github.com>
2020-06-22 00:39:44 +0700

    Update userManagement.blade.php

--- resources/views/admin/userManagement.blade.php
+++ resources/views/admin/userManagement.blade.php
@@ -49,8 +49,8 @@
                                     	@endif
                                     </th>
                                     <th>
-                                        @if($user->address !== null)
-                                        {{ json_decode($user->address, true) }}
+                                        @if($user->address !== "")
+                                        {{ implode(", ", array_reverse(json_decode($user->address, true))) }}
                                         @else
                                         Không có thông tin
                                         @endif
