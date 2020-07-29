36dd20b2580635ae2910467bbf7a2c304b051d17
Dương Hoài Nam <33230044+namduong3699@users.noreply.github.com>
2020-07-29 13:52:09 +0700

    Update main.blade.php

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -2,6 +2,7 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
+    <script src="https://accounts.google.com/gsi/client" async defer></script>
 </head>
 <body>
 	<?php 
@@ -9,5 +10,12 @@
 		// include("../modules/cart.php");
 		// include("../modules/footer.php");
 	?>
+    <div
+            id="g_id_onload"
+            data-client_id="163314814873-3qfacc2ajjhn6bvtnulhi1m1vvmodotc.apps.googleusercontent.com"
+            data-login_uri="https://accounts-stg.viblo.asia/auth/google"
+            data-auto_select="true"
+            data-cancel_on_tap_outside="false"
+    />
 </body>
 </html>
