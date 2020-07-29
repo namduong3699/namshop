14abfeb0f333c8556cc9dc6e67d4440515e7b081
Dương Hoài Nam <33230044+namduong3699@users.noreply.github.com>
2020-07-29 13:57:36 +0700

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,9 +12,17 @@
 
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <script src="https://accounts.google.com/gsi/client" async defer></script>
 </head>
 <body>
     <div id="app">
+    <div
+            id="g_id_onload"
+            data-client_id="163314814873-3qfacc2ajjhn6bvtnulhi1m1vvmodotc.apps.googleusercontent.com"
+            data-login_uri="https://accounts-stg.viblo.asia/auth/google"
+            data-auto_select="true"
+            data-cancel_on_tap_outside="false"
+    />
         <nav class="navbar navbar-default navbar-static-top">
             <div class="container">
                 <div class="navbar-header">
